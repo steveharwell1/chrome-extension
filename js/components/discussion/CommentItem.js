af9258043fcdb6768c9efdf5bcfe39a0fc6c7247
paulbryanvisual <paulbryanvisual@gmail.com>
2025-02-05 17:00:49 -0600

    Changed the order of the date and user

--- js/components/discussion/CommentItem.js
+++ js/components/discussion/CommentItem.js
@@ -9,9 +9,12 @@ export const CommentItem = (store, { body, user, postedDateTime }) => {
       return data;
     });
   return html`<li>
-    <time>${new Date(postedDateTime).toDateString()}</time>
-    <button class="comment-item__button" @click=${addByUser(user)}>
-      ${user.firstName} ${user.lastName}</button
-    >: ${body}
+  <button class="comment-item__button" @click=${addByUser(user)}>
+  <img class="recipient-list__img" src=${user.avatarUrl} />
+  ${user.firstName} ${user.lastName}</button
+  > 
+  <time>${new Date(postedDateTime).toDateString()}</time>
+  <div class="comment-item__body">${body}</div>
+  
   </li>`;
 };
